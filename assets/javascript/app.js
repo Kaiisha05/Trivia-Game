184555c336e4d1db3f1cbb8eddfe68477146bb2b
Kaiisha05 <Kaiisha@yahoo.com>
2019-09-24 22:50:22 -0400

    changed questions back to being built into the html. Added class element to all pages for easier css styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,11 @@ var numberWrong
 var numberSkipped
 var beginGame
 var endGame
+var quizTime = 225;
 
+$("start").on("click", start);
+$("submit").on("click", stop);
+$(".time-remaining").html(`Time Remaining:` + " <h2>");
 // var para = $("<p>")
 // trying to create paragraphs to hold the questions
 
@@ -11,41 +15,41 @@ var endGame
 // const quizQuestions = document.getElementById("questions")
 // doesn't seem neccessary to keep
 
-const myQuestions = [{
-    question:"What did Suttle call a fork?"
+// const myQuestions = [{
+//     question:"What did Suttle call a fork?"
 //     answers: {
 //         a: "Dinglehopper", b: "Snarfblatt", c: "Zygote", d: "Fork"
-    },
+    // },
 //     correctAnswer: "a"
 // },
-{
-    question:"What body part did Scuttle check on Prince Eric to confirm life?",
+// {
+//     question:"What body part did Scuttle check on Prince Eric to confirm life?",
 //     answers: {
 //         a: "Foot", b: "Wrist", c: "Chest", d: "Head"
-    },
+    // },
 //     correctAnswer: "a"
 // },
-{
-    question:"How many dozen eggs did Gaston eat each morning when he was a lad?",
-//     answers: {
+// {
+//     question:"How many dozen eggs did Gaston eat each morning when he was a lad?",
+// //     answers: {
 //         a: "Five", b: "Four", c: "Three", d: "What's a lad?"
 //     },
 //     correctAnswer: "b"
-},
-]
-console.log(myQuestions)
+// },
+// ]
+// console.log(myQuestions)
 // logs an array of the questions
 // var q = myQuestions
 // doesn't do anything
-var container = $("<div></div>");
+// var container = $("<div></div>");
 
-for (var q = 0; q < myQuestions.length; q++) {
+// for (var q = 0; q < myQuestions.length; q++) {
     // $("#questions").text(myQuestions.question) this does nothing
 
-console.log(myQuestions[q].question)
+// console.log(myQuestions[q].question)
 // logs the results from line 40 into the console..this function works!
-var $question = this.createNewQuestion(q);
-$question.appendTo($quizQuestions);
+// var $question = this.createNewQuestion(q);
+// $question.appendTo($quizQuestions);
 // para.append(myQuestions[q].question)
 // does the same thing as the above code
 //   $("#questions").append(myQuestions[q].question)
@@ -54,8 +58,8 @@ $question.appendTo($quizQuestions);
 // $("#questions").append(para)
 // does nothing new
 
-}
-$("#quiz-questions").html$(container)
+// }
+// $("#quiz-questions").html$(container)
 
 
 
