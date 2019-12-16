0f9030a6755080247ec8286528cedd64d845d32e
Kaiisha05 <Kaiisha@yahoo.com>
2019-12-16 15:22:18 -0500

    updated answer choices, forgot it was in an array so options should be 0-3 not 1-4

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,90 +11,14 @@ var quizTime = 15;
 // var beginGame
 var intervalId;
 var array = [];
+// var q = triviaQuestions.length;
 
 // $("#start").on("click", function () {
 //     $(".intro-container").hide();
 //     $(".question-container").show();
 //     run()
 // });
-
-/* -------------- QUESTIONS ------------- */
-const triviaQuestions = [{
-    question: "What did Scuttle call a fork?",
-    choices: ["Dinglehopper", "Snarfblatt", "Zygost", "Fork"],
-    answer: 1,
-},
-{
-    question: "What body part did Scuttle check on Prince Eric to confirm life?",
-    choices: ["Wrist", "Chest", "Head", "Foot"],
-    answer: 4,
-},
-{
-    question: "How many dozen of eggs did Gaston eat each morning when he was a lad?",
-    choices: ["Three", "Four", "Five", "What's a lad?"],
-    answer: 2,
-},
-{
-    question: "What color was Cinderella's original dress to the ball?",
-    choices: ["Pink", "Blue", "White", "Purple"],
-    answer: 1,
-},
-{
-    question: "Finish the lyrics: I know you I walked with you once upon a ______.",
-    choices: ["Blue Moon", "Dream", "Starry Night", "Time"],
-    answer: 2,
-},
-{
-    question: "Which dwarf doesn't belong?",
-    choices: ["Bashful", "Grumpy", "Doc", "Joyful"],
-    answer: 4,
-},
-{
-    question: "Pochantas was friends with a _____ and a _____.",
-    choices: ["Hummingbird, Possum", "Hummingbird, Raccoon", "Possum, Parrot", "Raccoon, Parrot"],
-    answer: 2,
-},
-{
-    question: "Moana's journey was to return Te Fiti's _____.",
-    choices: ["Chicken", "Fish Hook", "Heart", "Ring"],
-    answer: 3,
-},
-{
-    question: "hat animal did Princess Jasmine have as a pet?",
-    choices: ["Cheetah", "Jaguar", "Leopard", "Tiger"],
-    answer: 4,
-},
-{
-    question: "What was Tiana's dream?",
-    choices: ["Find true love", "Kiss a frog", "Marry a Prince", "Open a restaurant"],
-    answer: 4,
-},
-{
-    question: "Who joined the army in place of their dad?",
-    choices: ["Ariel", "Elsa", "Merida", "Mulan"],
-    answer: 4,
-},
-{
-    question: "Sven is a _____.",
-    choices: ["Prince", "Reindeer", "Snowman", "Troll"],
-    answer: 2,
-},
-{
-    question: "What color is Merida's hair?",
-    choices: ["Black", "Blonde", "Red", "White"],
-    answer: 3,
-},
-{
-    question: "What was Simba's mother's name?",
-    choices: ["Anna", "Jasmine", "Nala", "Sarabi"],
-    answer: 4,
-},
-{
-    question: "Who's hand does Walt Disney hold?",
-    choices: ["Mickey Mouse", "Minnie Mouse", "Donald Duck", "Goofy"],
-    answer: 1,
-}];
-/* ------------- FUNCTIIONS ------------- */
+/* -------------- START TRIVIA -------------- */
 $(document).ready(function () {
     $(".question-container").hide();
     $(".results-container").hide();
@@ -102,12 +26,91 @@ $(document).ready(function () {
         $("#intro-container").hide();
         startTrivia();
         run();
-        for (var i = 0; i < triviaQuestions.length; i++) {
+        for (var i = 0; i < trivia.length; i++) {
             /*research */
-            array.push(triviaQuestions[i]);
+            array.push(trivia[i]);
         }
     })
 });
+/* -------------- QUESTIONS ------------- */
+var trivia = [{
+    question: "What did Scuttle call a fork?",
+    choices: ["Dinglehopper", "Snarfblatt", "Zygost", "Fork"],
+    answer: 0,
+},
+{
+    question: "What body part did Scuttle check on Prince Eric to confirm life?",
+    choices: ["Wrist", "Chest", "Head", "Foot"],
+    answer: 3,
+},
+{
+    question: "How many dozen of eggs did Gaston eat each morning when he was a lad?",
+    choices: ["Three", "Four", "Five", "What's a lad?"],
+    answer: 1,
+},
+{
+    question: "What color was Cinderella's original dress to the ball?",
+    choices: ["Pink", "Blue", "White", "Purple"],
+    answer: 0,
+},
+{
+    question: "Finish the lyrics: I know you I walked with you once upon a ______.",
+    choices: ["Blue Moon", "Dream", "Starry Night", "Time"],
+    answer: 1,
+},
+{
+    question: "Which dwarf doesn't belong?",
+    choices: ["Bashful", "Grumpy", "Doc", "Joyful"],
+    answer: 3,
+},
+{
+    question: "Pochantas was friends with a _____ and a _____.",
+    choices: ["Hummingbird, Possum", "Hummingbird, Raccoon", "Possum, Parrot", "Raccoon, Parrot"],
+    answer: 1,
+},
+{
+    question: "Moana's journey was to return Te Fiti's _____.",
+    choices: ["Chicken", "Fish Hook", "Heart", "Ring"],
+    answer: 2,
+},
+{
+    question: "hat animal did Princess Jasmine have as a pet?",
+    choices: ["Cheetah", "Jaguar", "Leopard", "Tiger"],
+    answer: 3,
+},
+{
+    question: "What was Tiana's dream?",
+    choices: ["Find true love", "Kiss a frog", "Marry a Prince", "Open a restaurant"],
+    answer: 3,
+},
+{
+    question: "Who joined the army in place of their dad?",
+    choices: ["Ariel", "Elsa", "Merida", "Mulan"],
+    answer: 3,
+},
+{
+    question: "Sven is a _____.",
+    choices: ["Prince", "Reindeer", "Snowman", "Troll"],
+    answer: 1,
+},
+{
+    question: "What color is Merida's hair?",
+    choices: ["Black", "Blonde", "Red", "White"],
+    answer: 2,
+},
+{
+    question: "What was Simba's mother's name?",
+    choices: ["Anna", "Jasmine", "Nala", "Sarabi"],
+    answer: 3,
+},
+{
+    question: "Who's hand does Walt Disney hold?",
+    choices: ["Mickey Mouse", "Minnie Mouse", "Donald Duck", "Goofy"],
+    answer: 0,
+}];
+console.log(trivia)
+/* ------------- FUNCTIIONS ------------- */
+
 /* ------------- TIMER------------- */
 function run() {
     clearInterval(intervalId);
@@ -122,24 +125,27 @@ function decrement() {
     quizTime--;
     $("#time-remaining").html("<h2>" + "Time Remaining: " + quizTime + "</h2>");
     if (quizTime === 0) {
-        numberSkipped ++;
+        numberSkipped++;
         $("question-container").html("<h2>" + "Oh no, time's up, the correct answer is: " + triviaQuestions.choice[triviaQuestions.answer] + "</h2>");
         countdown = false;
         stop();
-    }
-}
+    };
+};
 
 /* ------------- DISPLAY QUESTIONS ------------- */
 
-startTrivia () {
-var shuffle = Math.floor(Math.random()*triviaQuestions.length;
-userSelection=triviaQuestions[shuffle];
-$("#questions").html("<h2>" + userSelection.question + "</h2>");
-for var i = 0; i < userSelection.choice.length; i++) {
-    
-}
-)
-}
+// function startTrivia() {
+//     var shuffle = Math.floor(Math.random() * trivia.question.length);
+//     userSelection = trivia.question[shuffle];
+//     $("#questions").html("<h2>" + userSelection.question + "</h2>");
+//     for (var i = 0; i < userSelection.choice.length; i++) {
+//         var selection = $("<div>");
+//         selection.addClass("selected");
+//         selection.html(userSelection.choices[i]);
+//         userSelection.attr("data-guess", i);
+//         $(".question-container").append(selection);
+//     };
+// };
 
 
 
@@ -148,6 +154,7 @@ $("#submit").click(function () {
     $(".results-container").show();
 });
 
+// startTrivia();
 // need functionality for PLAY AGAIN BUTTON
 
 
